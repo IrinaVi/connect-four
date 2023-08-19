9ca2e92de043f6af78302631b870c30b11c3af51
IrinaVi <IrinaVi>
2023-08-19 21:59:52 +0100

    Add conditionals depending on who is playing

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded') {
-    
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].onclick = () => {
+            if (squares[i + 7].classList.contains('taken')) {
+                if (currentPlayer == 1) {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-one')
+                    currentPlayer = 2
+                    displayCurrentPlayer.innderHTML = currentPlayer
+                } if (currentPlayer == 2) {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-two')
+                    currentPlayer = 1
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                }
+            }
+        }
+    }
+})
